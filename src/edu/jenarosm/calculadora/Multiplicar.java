59c171c147f557584a98eda7a5720e8944c86d7f
mychob <jenarosm@gmail.com>
2021-05-28 14:43:01 +0200

    Implementación del metodo multiplica()

--- src/edu/jenarosm/calculadora/Multiplicar.java
+++ src/edu/jenarosm/calculadora/Multiplicar.java
@@ -1,5 +1,17 @@
 package edu.jenarosm.calculadora;
+import edu.jenarosm.calculadora.Suma;
+
 
 public class Multiplicar {
-
+	
+	public int multiplica(int x, int y) {
+		Suma s = new Suma();
+		int total = 0;
+		
+		for (int i=0;i<x;i++) {
+			total=s.suma(total, y);
+		}
+		return total;
+	}
+	
 }
