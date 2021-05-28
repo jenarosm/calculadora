c2374745e06bd86f108e80db64d1432a072667c6
mychob <jenarosm@gmail.com>
2021-05-28 15:01:30 +0200

    Implementación de pruebas y arreglos en multiplicar()

--- src/edu/jenarosm/calculadora/Multiplicar.java
+++ src/edu/jenarosm/calculadora/Multiplicar.java
@@ -7,11 +7,31 @@ public class Multiplicar {
 	public int multiplica(int x, int y) {
 		Suma s = new Suma();
 		int total = 0;
+		boolean isNeg = false;
+		
+		if(x==0 || y == 0) {
+			return 0;
+		}else if (x<0 && y<0) {
+			isNeg = false;
+			x=-x;
+			y=-y;
+		}else if(x<0) {
+			isNeg = true;
+			x=-x;
+		}else if(y<0) {
+			isNeg = true;
+			y=-y;
+		}
 		
 		for (int i=0;i<x;i++) {
 			total=s.suma(total, y);
 		}
-		return total;
+		
+		if (!isNeg) {
+			return total;
+		}else {
+			return -total;
+		}
 	}
 	
 }
--- src/edu/jenarosm/calculadora/TestJunit.java
+++ src/edu/jenarosm/calculadora/TestJunit.java
@@ -6,9 +6,23 @@ import static org.junit.Assert.assertEquals;
 public class TestJunit {
 	
    Suma sum = new Suma();
-
+   Multiplicar mult = new Multiplicar();
+   
    @Test
-   public void testPrintMessage() {	  
+   public void testSuma() {	  
       assertEquals(4,sum.suma(2,2));
    }
+   
+   @Test
+   public void testMultiplica() {	  
+      assertEquals(8,mult.multiplica(2, 4));
+      assertEquals(0,mult.multiplica(0, 4));
+      assertEquals(0,mult.multiplica(4, 0));
+      assertEquals(-9,mult.multiplica(3, -3));
+      assertEquals(-15,mult.multiplica(-5, 3));
+      assertEquals(12,mult.multiplica(-6, -2));
+   }
+   
+
+   
 }
